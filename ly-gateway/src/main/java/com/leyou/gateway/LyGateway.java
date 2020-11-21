d38e85a19433383439d84f9e397cb3c8912f6bc8
liuxing <578941470@qq.com>
2020-11-21 19:15:58 +0800

    网关微服务配置修改

--- ly-gateway/src/main/java/com/leyou/gateway/LyGateway.java
+++ ly-gateway/src/main/java/com/leyou/gateway/LyGateway.java
@@ -2,6 +2,7 @@ package com.leyou.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 
@@ -11,7 +12,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulServer;
  * @description:
  * @date 2020/11/20 19:44
  */
-@SpringBootApplication
+@SpringCloudApplication
 @EnableZuulProxy
 public class LyGateway {
     public static void main(String[] args) {
